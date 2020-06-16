a22db326a93e21150277d566d763b905e5ac1079
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-06-17 02:09:17 +0530

    Updates

---
+++ precache-manifest.26857c17380af783cece20bd60e877f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ec67c0df30af8236691fa23e9e4ad0",
+    "url": "/styled-login-page/index.html"
+  },
+  {
+    "revision": "8839c4ffcf69f5a6a5f1",
+    "url": "/styled-login-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f02cc679220eeb07466d",
+    "url": "/styled-login-page/static/js/2.149abc51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styled-login-page/static/js/2.149abc51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8839c4ffcf69f5a6a5f1",
+    "url": "/styled-login-page/static/js/main.975bba6a.chunk.js"
+  },
+  {
+    "revision": "efd25ac52b2852a6d846",
+    "url": "/styled-login-page/static/js/runtime-main.e149a320.js"
+  }
+]);
